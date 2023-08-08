f25f102b410ad9bc2f6b8f95bcb054734b1e284a
Tulio <tulio.mminini@gmail.com>
2023-08-08 20:09:33 +0100

    :lipstick::construction: CalcKeypad css starte and calculator buttons created

--- src/components/CalcKeypad/CalcKeypad.jsx
+++ src/components/CalcKeypad/CalcKeypad.jsx
@@ -1,9 +1,33 @@
 import style from "./CalcKeypad.module.css";
 
 export function CalcKeypad() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("The button is clicked");
+  };
+
   return (
-    <div className={style.keypadContainer}>
-      <p>Calculator Keypad</p>
-    </div>
+    <main className={style.keypadContainer}>
+      <div className={style.buttonsContainer}>
+        <button onClick={handleClick}>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>del</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button>/</button>
+        <button>x</button>
+        <button>reset</button>
+        <button>=</button>
+      </div>
+    </main>
   );
 }
