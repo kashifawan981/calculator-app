6e4cf9b3f9960c9196300c833e39479f352fb455
Tulio <tulio.mminini@gmail.com>
2023-08-09 22:36:31 +0100

    :lipstick: CalcButton css started and almost finished

--- src/components/CalcKeypad/CalcButton.jsx
+++ src/components/CalcKeypad/CalcButton.jsx
@@ -3,9 +3,26 @@ import style from "./CalcButton.module.css"
 import React from "react";
 
 export const CalcButton = ({ value, onClick }) => {
+
+
+  const specialButtons = ['RESET', 'DEL', '='];
+  let buttonClass;
+
+  if (specialButtons.includes(value)) {
+    if (value === 'RESET') {
+      buttonClass = style.blueButtons;
+    } else if (value === 'DEL') {
+      buttonClass = style.blueButtons;
+    } else if (value === '=') {
+      buttonClass = style.redButton;
+    }
+  } else {
+    buttonClass = style.regButton;
+  }
+
   return (
     <div >
-      <button className={style.button} onClick={() => onClick(value)}>
+      <button className={buttonClass} onClick={() => onClick(value)}>
         {value === "*" ? "x" : value}
       </button>
     </div>
