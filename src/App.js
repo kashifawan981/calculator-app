35635bf6e31abc643e27be99e3447d3fb9dd7eb6
Tulio <tulio.mminini@gmail.com>
2023-11-30 14:57:23 +0000

    code improved and comments added

--- src/App.js
+++ src/App.js
@@ -1,57 +1,81 @@
-import style from './App.module.css';
-
-import React, { useEffect, useState } from 'react';
 import * as math from 'mathjs';
-
+import style from './App.module.css';
+import React, { useEffect, useState } from 'react';
 import { CalcKeypad } from './components/CalcKeypad/CalcKeypad';
 import { CalcScreen } from './components/CalcScreen/CalcScreen';
 import { CalcThemes } from './components/CalcThemes/CalcThemes';
 
 function App() {
+  // State variables for calculation, result display, and theme.
   const [calculation, setCalculation] = useState('');
   const [isResultDisplayed, setIsResultDisplayed] = useState(false);
   const [theme, setTheme] = useState('theme1');
 
-  useEffect(() => {
-    // Set default theme when the component mounts
-    document.body.className = theme;
-  }, [theme]); // <-- Dependency array to watch for changes in 'theme'
+  // Function to update the body class based on the selected theme.
+  const updateBodyClass = (selectedTheme) => {
+    document.body.className = selectedTheme;
+  };
 
+  // useEffect hook to update the body class when the theme changes.
+  useEffect(() => {
+    updateBodyClass(theme);
+  }, [theme]);
+
+  // Handler function for button clicks.
   const handleButtonClick = (value) => {
     if (isResultDisplayed && !isNaN(value) && value !== '.') {
+      // If result is displayed and the clicked value is a number, start a new calculation.
       setCalculation(value);
       setIsResultDisplayed(false);
     } else {
-      if (value === '=') {
-        try {
-          const result = math.evaluate(
-            calculation.replace(',', '.').replace('x', '*')
-          );
-          setCalculation(result.toString());
-          setIsResultDisplayed(true);
-        } catch (error) {
-          setCalculation('Error');
-        }
-      } else if (value === '<=') {
-        handleDelete();
-      } else if (value === 'C') {
-        handleReset();
-      } else {
-        setCalculation(calculation + value.replace(',', '.'));
-        setIsResultDisplayed(false);
-      }
+      // Handle different button actions.
+      value === '='
+        ? handleEquals()
+        : value === '<='
+        ? handleDelete()
+        : value === 'C'
+        ? handleReset()
+        : handleRegularButton(value);
     }
   };
 
+  // Handler function for equals button (=).
+  const handleEquals = () => {
+    try {
+      // Evaluate the calculation and display the result.
+      const result = math.evaluate(
+        calculation.replace(',', '.').replace('x', '*')
+      );
+      setCalculation(result.toString());
+      setIsResultDisplayed(true);
+    } catch (error) {
+      // Handle errors during evaluation.
+      setCalculation('Error');
+    }
+  };
+
+  // Handler function for regular buttons (numbers and operators).
+  const handleRegularButton = (value) => {
+    // Append the clicked value to the current calculation
+    setCalculation(calculation + value.replace(',', '.'));
+    setIsResultDisplayed(false);
+  };
+
+  // Handler function for delete button (<=).
   const handleDelete = () => {
+    // Remove the last character from the calculation.
     setCalculation(calculation.slice(0, -1));
   };
 
+  // Handler function for reset button (C).
   const handleReset = () => {
+    // Reset the entire calculation.
     setCalculation('');
   };
 
+  // Handler function for theme change.
   const handleThemeChange = (newTheme) => {
+    // Update the theme when the user selects a different theme.
     setTheme(newTheme);
   };
 
